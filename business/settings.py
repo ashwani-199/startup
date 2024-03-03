c562df1fa240929ee213c2030d9c0901f644113a
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-03-03 20:05:59 +0530

    Update settings.py

--- business/settings.py
+++ business/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -146,8 +146,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 #     # and renames the files with unique names for each version to support long-term caching
 #     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATICFILES_DIRS = [BASE_DIR / "static"] #new 
-# STATIC_ROOT = BASE_DIR / "staticfiles"  #new
+STATICFILES_DIRS = [BASE_DIR / "static"] #new 
+STATIC_ROOT = BASE_DIR / "staticfiles"  #new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
