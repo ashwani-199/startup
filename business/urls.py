db7a954dbb1dd95240accde3d89b69d8aa59bc75
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-03-03 18:02:05 +0530

    Update urls.py

--- business/urls.py
+++ business/urls.py
@@ -40,7 +40,7 @@ if settings.DEBUG:
         path('quote/', include('apps.free_quote.urls')),
         path('team/', include('apps.team_members.urls')),
         path('testimonial/', include('apps.testimonial.urls')),
-    # ]
+    ]
     
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
