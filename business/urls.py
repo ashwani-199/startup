646da9f33cb5f71515493faab72ca940146413f4
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-03-10 12:29:31 +0530

    Update urls.py

--- business/urls.py
+++ business/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.site.site_title = "StartUp site admin"
@@ -39,7 +39,7 @@ urlpatterns = [
     path('quote/', include('apps.free_quote.urls')),
     path('team/', include('apps.team_members.urls')),
     path('testimonial/', include('apps.testimonial.urls')),
-]
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
