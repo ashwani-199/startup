0ec0716db6c4e1362ca2fa6dc27b4bb9c582f887
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-05-04 14:42:40 +0530

    set

--- business/settings.py
+++ business/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # STATIC_ROOT = BASE_DIR / "static"
 
