b878bef848055465892bcc217cb6820507241cd6
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-05-04 15:38:07 +0530

    evening2024

--- business/urls.py
+++ business/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.site.site_title = "StartUp site admin"
@@ -42,6 +42,6 @@ urlpatterns = [
 
     path('summernote/', include('django_summernote.urls')),
 
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]
+# urlpatterns += staticfiles_urlpatterns()
