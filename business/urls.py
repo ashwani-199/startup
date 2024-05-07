1a92c1347ef24bfc82ccecb87b7bab471f65a10b
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-05-07 20:40:15 +0530

    urls202407

--- business/urls.py
+++ business/urls.py
@@ -40,7 +40,6 @@ handler400 = MainService.error_400
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += staticfiles_urlpatterns()
