69701d3b57f4c02d8e7772fedb263fdef6d4a9c3
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-05-08 18:06:07 +0530

    evening082024

--- business/settings.py
+++ business/settings.py
@@ -134,35 +134,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT =  BASE_DIR / "media"
+MEDIA_URL = 'media/'
+MEDIA_ROOT =  BASE_DIR / "media"
 
 # STATIC_ROOT = BASE_DIR / "static"
 
-# if not DEBUG:
-#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "static"] #new 
+STATIC_ROOT = BASE_DIR / "staticfiles"  #new
 
-# STATICFILES_DIRS = [BASE_DIR / "static"] #new 
-# STATIC_ROOT = BASE_DIR / "staticfiles"  #new
-
-
-# New Staticfiles
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = BASE_DIR / "media"
-
-# if DEBUG:
-#     STATIC_ROOT = BASE_DIR/ "static"
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
