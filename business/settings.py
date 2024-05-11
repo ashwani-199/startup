4889c8612d42355219b38d1a19570d357a023fbc
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-05-11 11:40:10 +0530

    Agin2024

--- business/settings.py
+++ business/settings.py
@@ -134,14 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  BASE_DIR / "media"
 
 # STATIC_ROOT = BASE_DIR / "static"
 
-# STATICFILES_DIRS = [BASE_DIR / "static"] #new 
+STATICFILES_DIRS = [BASE_DIR / "static"] #new 
 STATIC_ROOT = BASE_DIR / "staticfiles"  #new
 
 
